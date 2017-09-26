afa351339f3333b9f3207d68039bf3b330ccffc0
mayumi chiba <mayumi183@gmail.com>
2017-09-26 04:43:32 +0000

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ SampleApp::Application.routes.draw do
     :registrations => "registrations"
   }
   resources :users, only: [:show, :index, :destroy]
-  root  'static_pages#home'
+  resources :microposts, only: [:create, :destroy]
  
-
+  root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
